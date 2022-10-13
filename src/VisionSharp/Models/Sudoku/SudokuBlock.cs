82dc207b6e3aff283e39723fec7a1b8e789bf7b4
Xin.Pu <pu.xin@outlook.com>
2022-10-13 12:24:07 +0800

    feat: add sudoku solver and detector

---
+++ src/VisionSharp/Models/Sudoku/SudokuBlock.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace VisionSharp.Models.Sudoku
+{
+    /// <summary>
+    ///     数独单元格
+    /// </summary>
+    public class SudokuBlock : ObservableObject
+    {
+        public SudokuBlock(bool isCondition, byte number, byte column, byte row)
+        {
+            IsCondition = isCondition;
+
+            Number = number;
+            Column = column;
+            Row = row;
+        }
+
+        /// <summary>
+        ///     填入的数字
+        /// </summary>
+        public byte Number { set; get; }
+
+        /// <summary>
+        ///     X坐标
+        /// </summary>
+        public byte Column { get; }
+
+        /// <summary>
+        ///     Y坐标
+        /// </summary>
+        public byte Row { get; }
+
+
+        /// <summary>
+        ///     是否为条件（题目）给出数字的单元格
+        /// </summary>
+        public bool IsCondition { get; }
+
+
+        public void SetNumber(byte number)
+        {
+            Number = number;
+        }
+
+        public override string ToString()
+        {
+            return Number == 0 ? "." : Number.ToString();
+        }
+    }
+}
---
+++ src/VisionSharp/Processor/Slovers/SudokuDetector.cs
@@ -0,0 +1,20 @@
+﻿using OpenCvSharp;
+using VisionSharp.Models.Sudoku;
+
+namespace VisionSharp.Processor.Slovers
+{
+    public class SudokuDetector : FeatureExtractor<SudokuSubject>
+    {
+        /// <summary>
+        ///     数独检测器
+        /// </summary>
+        public SudokuDetector() : base("SudokuDetector")
+        {
+        }
+
+        internal override SudokuSubject Process(Mat input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
