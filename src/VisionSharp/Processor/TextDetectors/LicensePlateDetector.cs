fdffebcd20ebb0b92ba413ce47028d91a2315473
Xin.Pu <pu.xin@outlook.com>
2022-10-13 17:16:52 +0800

    feat: add classifier and license plate detector stubs

---
+++ src/VisionSharp/Processor/TextDetectors/LicensePlateDetector.cs
@@ -0,0 +1,24 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Dnn;
+
+namespace VisionSharp.Processor.TextDetectors
+{
+    /// <summary>
+    ///     车牌检测器识别网络
+    ///     Todo
+    /// </summary>
+    /// <remarks>https://github.com/sirius-ai/LPRNet_Pytorch</remarks>
+    /// <remarks>https://arxiv.org/abs/1806.10447v1</remarks>
+    public class LicensePlateDetector : TextDetector
+    {
+        public LicensePlateDetector(string pth) : base("LicensePlateDetector")
+        {
+            var d = CvDnn.ReadNetFromTorch(pth);
+        }
+
+        internal override string Process(Mat input)
+        {
+            return string.Empty;
+        }
+    }
+}
