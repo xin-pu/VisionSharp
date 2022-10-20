04164f263e1e412055d5d44d08c44e5bdcd1fdc7
xin-pu <pu.xin@outlook.com>
2022-10-20 09:11:19 +0800

    change solution.

--- src/VisionSharp/Models/Category/MudCategory.cs
+++ src/VisionSharp/Models/Category/MudCategory.cs
@@ -8,16 +8,11 @@
         /// <summary>
         ///     位置没有产品
         /// </summary>
-        Empty = 0,
+        Other = 0,
 
         /// <summary>
         ///     位置有产品，但丢失散热泥
         /// </summary>
-        MissMud = 1,
-
-        /// <summary>
-        ///     位置有产品，并且有散热泥
-        /// </summary>
-        WithMud = 2
+        MissMud = 1
     }
 }
