61a1d7336f16da02434efb2e06eefb88158c682f
Xin.Pu <pu.xin@outlook.com>
2023-05-15 13:42:46 +0800

    refactor: split object detector by yolo version

---
+++ src/UnitTest/ProcessorTest/Yolo/Yolo3OnnxTest.cs
@@ -0,0 +1,31 @@
+﻿using OpenCvSharp.Dnn;
+using Xunit.Abstractions;
+
+namespace UnitTest.ProcessorTest.Yolo
+{
+    public class Yolo3OnnxTest : AbstractTest
+    {
+        public Yolo3OnnxTest(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
+        {
+        }
+
+        [Fact]
+        public void ObjDetectortTest()
+        {
+            var model = @"F:\SaveModels\Yolo\raccoon-tiny.onnx";
+            var imageFilePath = @"..\..\..\..\testimages\002341.jpg";
+            var d = CvDnn.ReadNetFromOnnx(model).GetLayerNames();
+            PrintObject(string.Join("\r", d));
+        }
+
+        [Fact]
+        public void ObjDetectortTestID()
+        {
+            var model = @"F:\SaveModels\Yolo\raccoon-tiny.onnx";
+            var imageFilePath = @"..\..\..\..\testimages\002341.jpg";
+            var d = CvDnn.ReadNetFromOnnx(model).GetLayerId("532");
+            PrintObject(d);
+        }
+    }
+}
