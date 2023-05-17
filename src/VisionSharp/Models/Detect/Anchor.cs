38350dcea44bdada4f3aef2400b4595d106618b2
Xin.Pu <pu.xin@outlook.com>
2023-05-17 14:24:23 +0800

    feat: add anchor grid utilities

---
+++ src/VisionSharp/Models/Detect/Anchor.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace VisionSharp.Models.Detect
+{
+    public class Anchor : ObservableObject
+    {
+        private float _wdith;
+        private float _height;
+
+
+        public Anchor()
+        {
+        }
+
+        public Anchor(float wdith, float height)
+        {
+            Width = wdith;
+            Height = height;
+        }
+
+        public float Width
+        {
+            internal set => SetProperty(ref _wdith, value);
+            get => _wdith;
+        }
+
+        public float Height
+        {
+            internal set => SetProperty(ref _height, value);
+            get => _height;
+        }
+    }
+}
